83a01c255d8a4fb78fbe1f371564450395b99530
阮书浩 <14543029+shuhaoruan@user.noreply.gitee.com>
2024-08-09 00:04:52 +0800

    Update Mongo Connectino String

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/requests', requestRoutes);
 app.use('/api/applications', applicationRoutes);
 
-mongoose.connect('your_mongo_connection_string', {
+const mongoURI = 'mongodb+srv://ShuhaoR:Bobby1024%21@shuhao-startup.nubp8.mongodb.net/?retryWrites=true&w=majority&appName=Shuhao-Startup';
+
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.listen(5000, () => console.log('Server running on port 5000'));
 
