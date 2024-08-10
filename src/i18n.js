3dfa6dd6a57482f25f1287ee519494095deb8d25
阮书浩 <14543029+shuhaoruan@user.noreply.gitee.com>
2024-08-11 06:39:36 +0800

    Set default to english

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,9 @@
-// src/i18n.js
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import translationEN from "./locales/en/translation.json";
-import translationZH from "./locales/zh/translation.json";
+import translationZH from "./locales/zh/translation.json"; // Assuming zh-tw was changed to zh
 import translationJP from "./locales/ja/translation.json";
 
 const resources = {
@@ -19,7 +18,8 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    fallbackLng: "en",
+    lng: localStorage.getItem("i18nextLng") || "en", // Default to English if no language is set
+    fallbackLng: "en", // Fallback to English
     debug: true,
     interpolation: {
       escapeValue: false,
