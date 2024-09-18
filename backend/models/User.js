2bdbcaab08e936ea0b83dfe1edd61b6ce3b3e270
阮书浩 <14543029+shuhaoruan@user.noreply.gitee.com>
2024-09-17 21:23:50 -0400

    Added role field to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,10 @@
-// backend/models/User.js
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: String, enum: ["user", "employee"], required: true }, // Added role field
   date: { type: Date, default: Date.now },
 });
 
